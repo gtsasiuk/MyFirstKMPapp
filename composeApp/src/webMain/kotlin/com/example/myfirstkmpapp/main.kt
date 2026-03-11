4257289edce639656aec040ac76f9d023392a2c5
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-03-11 13:09:18 +0200

    Initial commit

---
+++ composeApp/src/webMain/kotlin/com/example/myfirstkmpapp/main.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstkmpapp
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/myfirstkmpapp/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.myfirstkmpapp
+
+const val SERVER_PORT = 8080
